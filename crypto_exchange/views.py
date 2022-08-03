a833ab3672991a9cf78a5128e6fd8e66b5f7db97
Alejo Rios <alejoriosiglesias@gmail.com>
2022-08-03 16:26:04 +0100

    feat: homepage ready

--- crypto_exchange/views.py
+++ crypto_exchange/views.py
@@ -1,12 +1,17 @@
 from flask import render_template
 
 from . import app
+from .models import DBManager
+
+ROUTE = 'data/cryptovest.db'
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    db = DBManager(ROUTE)
+    movements = db.query_SQL("SELECT * FROM cryptovest")
+    return render_template("home.html", movs=movements)
 
-@app.route('/purchase', methods=['GET', "POST"])
+@app.route('/buy', methods=['GET', "POST"])
 def buy():
     return "What would you like to buy?"
 
